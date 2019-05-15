31e45e16df225f90fb631b0cd852283f007c50f7
Patrick Faustino Camello <terceiro-pcamello@icatuseguros.com.br>
2019-05-15 15:48:57 -0300

    Classe Collision2D documentada

--- Scripts/Math/Collide2D.js
+++ Scripts/Math/Collide2D.js
@@ -1,6 +1,29 @@
+/**
+ * @doc Class Collide2D
+ * @namespace Window
+ * @class Collide2D
+ * @author Patrick Faustino Camello
+ * @summary That class was made, to compose the EngineHtml5 framework.
+ * @Date 15/05/2019
+ * @example
+ *  var collide = new Collide2D();
+ *  if (collide.isCollidingAABB(obj1, obj2)) {
+ *      console.log("Collision");
+ *  }
+ * @returns {Object}
+ */
 class Collide2D extends Math {
     constructor() {}
 
+    /**
+     * @doc Method
+     * @param {obj1} rect 
+     * @param {obj2} otherRect
+     * @description Collision of type AABB, rectangular.
+     * @example
+     *  var collide = collide2D.isCollidingAABB(obj1, obj2);
+     * @returns {boolean}
+     */
     static isCollidingAABB(rect, otherRect) {
         var isCollision = false;
         if (otherRect.x <= rect.x + rect.width && otherRect.x + otherRect.width >= rect.x &&
@@ -9,6 +32,16 @@ class Collide2D extends Math {
         }
         return isCollision;
     }
+
+    /**
+     * @doc Method
+     * @param {obj1} circle 
+     * @param {obj2} obj
+     * @description Collision of type circular.
+     * @example
+     *  var collide = collide2D.isCollidingCircle(obj1, obj2);
+     * @returns {boolean} 
+     */
     static isCollidingCircle(circle, obj) {
         var isCollision = false;
         // Teorema de Pitagoras
